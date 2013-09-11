a8dc2af6e7857a805c73c921ab2233f2074523fe
Charlton Galvarino <charlton@2creek.com>
2013-09-11 16:31:28 -0400

    use wget for post.php

--- post.php
+++ post.php
@@ -1,21 +1,21 @@
 <?php
   set_time_limit(120);
 
-  $r = file_get_contents(
-     $_REQUEST['url']
-    ,false
-    ,stream_context_create(array('http' => array(
-       'method'  => 'POST'
-      ,'header'  => 'Content-type: text/xml'
-      ,'content' => file_get_contents('php://input')
-    )))
-  );
+  header ("Content-Type:text/xml");
+  $r = file_get_contents(wget($_REQUEST['url'],file_get_contents('php://input')));
 
   // DomQuery doesn't support namespaces -- BOO!
   // so swap out the : after each one we care about with a _
-  // 
+  //
   //   e.g. the ns param might be something like 'srv|gmd|gco'
 
-  header ("Content-Type:text/xml");
   echo preg_replace('/('.$_REQUEST['ns'].'):/','\1_',$r);
+
+  function wget($u,$p) {
+    $request  = '/tmp/'.rand().time().'.post.xml';
+    $response = '/tmp/'.rand().time().'.xml';
+    file_put_contents($request,$p);
+    `wget --header "Content-Type:text/xml" --post-file=$request '$u' -O $response > /dev/null 2>&1`;
+    return $response;
+  }
 ?>
